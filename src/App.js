3b02338b102a41d9ac06733c988b379e72c2d1fc
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2024-05-13 22:48:53 +0200

    chore: Update react-router-dom import in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/HomePage.js';
 import MovieDetailsPage from './pages/MovieDetailsPage.js';
 import TvShowDetailsPage from './pages/TvShowDetailsPage.js';
 import NotFoundPage from './pages/NotFoundPage.js';
-
 
 function App() {
   return (
