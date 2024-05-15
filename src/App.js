e066f5e66cedd25e27b81d8153b774bd7e715460
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2024-05-15 22:34:56 +0200

    refactor: Update routing paths in App.js and MovieDetailsPage.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HomePage from "./pages/HomePage.js";
 import MovieDetailsPage from "./pages/MovieDetailsPage.js";
 import TvShowDetailsPage from "./pages/TvShowDetailsPage.js";
 import NotFoundPage from "./pages/NotFoundPage.js";
-import "./App.css";
+import HomePage from "./pages/HomePage.js";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/movie/:id" element={<MovieDetailsPage />} />
-        <Route path="/tv/:id" element={<TvShowDetailsPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+        <Route path="/tv-shows/:tvShowId" element={<TvShowDetailsPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,12 @@
 import React from "react";
-import MovieCard from "../components/MovieCard";
+import TrendingMoviesCard from "../components/TrendingMoviesCard";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Movie Explorer</h1>
       <h2>Trending Movies</h2>
-      <MovieCard />
+      <TrendingMoviesCard />
     </div>
   );
 };
